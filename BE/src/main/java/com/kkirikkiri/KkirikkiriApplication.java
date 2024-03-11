0aaa7d387146460ea50f83e9a3c84a4fba853f5b
Juhyun Lee <jennysophia3@naver.com>
2024-03-11 10:25:21 +0900

    [Back] 초기 세팅

---
+++ BE/src/main/java/com/kkirikkiri/KkirikkiriApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a14a8bafff011dd30fcca9d239635a1d1df7c0cf0d1d897b149bec7d96c29418
+size 315
---
+++ BE/src/test/java/com/kkirikkiri/KkirikkiriApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ef00d08577eab15dd4e30cae58626a6ef1bd3ea2543e252cd860c766ab1fc96
+size 210
