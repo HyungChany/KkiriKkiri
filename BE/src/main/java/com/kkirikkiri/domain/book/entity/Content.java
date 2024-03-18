1528204067e5f0562754b49c12867fe441b97bf1
Juhyun Lee <jennysophia3@naver.com>
2024-03-18 16:55:07 +0900

    [Back] feat: 동화책 entity 완성

---
+++ BE/src/main/java/com/kkirikkiri/domain/book/entity/Content.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5336584004b8498033df099755abf2901777109c05fe2aba412035e31fdb1c15
+size 886
---
+++ BE/src/main/java/com/kkirikkiri/domain/book/entity/Story.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80458262e9768f54f9eaa9a910f9fa853bfcf54c281bb127374266b8d246fe86
+size 880
