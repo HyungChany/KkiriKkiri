3f2b7ebabd6998a261ba903afba665f553c13a1d
dbtywi <dbtywi@naver.com>
2024-03-19 16:07:07 +0900

    [Back] feat: 아이디, 닉네임 중복 검사 API 개발 완료
    Former-commit-id: 911fa3435f62bb23bf17c10b6acf9d09a92d9c28

--- BE/src/main/java/com/kkirikkiri/domain/member/repository/MemberRepository.java
+++ BE/src/main/java/com/kkirikkiri/domain/member/repository/MemberRepository.java
@@ -1,19 +1,3 @@
-package com.kkirikkiri.domain.member.repository;
-
-import com.kkirikkiri.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    @Override
-    Optional<Member> findById(Long id);
-
-    Optional<Member> findByLoginId(String loginId);
-
-    @Override
-    void deleteById(Long id);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6ac08013b221095aee1af0e6832b5fd6d66b0729536ba3fc6951bcfed4d09ea
+size 561
--- BE/src/main/java/com/kkirikkiri/domain/member/service/MemberService.java
+++ BE/src/main/java/com/kkirikkiri/domain/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ed2d9075df763ab7f29ce6b9a34e6cbeb25372e9dea9561a56044306ac6512b
-size 4083
+oid sha256:8d49085d31892e9c63602e73d8995d8e869555c2f63d0a129b3c7b69682afbd2
+size 4318
