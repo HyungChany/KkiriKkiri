1df1742aa3071b913273242211df0642a9b633a0
조형찬 <bd3945@naver.com>
2024-03-20 14:57:12 +0900

    feat: 코드 수정
    Former-commit-id: 64c35f339e6e4ddc24043f1d63ccef8102ae03bc

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db3a0783e6850740c510d94e6d8ba3d146f25f10fd5eb97865791938afaaa7b0
-size 1020
+oid sha256:13267e8dcb5bd1cda68bfa606166f598a2f276e89b56e82e99a3f8b642cf75f8
+size 1088
--- FE/frontend/src/pages/SignupPage.jsx
+++ FE/frontend/src/pages/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7446f08f71f6312c7efe82f59c5ae1adfcc95ad86838c0684c1a32050255f641
-size 1834
+oid sha256:951322ebec1a7d296d4e358dfb45bb5b305f94accf2b83b5ebfc58887b980c4c
+size 1711
