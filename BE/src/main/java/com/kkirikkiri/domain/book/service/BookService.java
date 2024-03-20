b20e6397a899b3178c21e2e7abca072c5317cd26
Juhyun Lee <jennysophia3@naver.com>
2024-03-20 11:01:35 +0900

    [Back] feat: redis 설정, 적용
    Former-commit-id: fe57c06d1cee222e80e33aa0bc9c628ebadb6fe6

--- BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08a843e21cab5b3ad668ddc76b8534bf0240fa980afad0e6912d76e0e5ddd493
-size 7431
+oid sha256:69e6b586c93193620ea12b96660e0666bf4088cd06482ec4798e2aa2cba31f4a
+size 7480
