a33270e55ff6d8fbceb9c8d713e04472b94056bb
이주현 <jennysophia3@naver.com>
2024-03-20 10:06:45 +0900

    Update BookService.java
    Former-commit-id: b6a8f6c569a0a515ed7ab21d107d967d3ea7a995

--- BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49a91a9f296b3e358d4510a8e896e1cbb288a72bfed56e76a589b3c183617bf2
-size 7428
+oid sha256:08a843e21cab5b3ad668ddc76b8534bf0240fa980afad0e6912d76e0e5ddd493
+size 7431
