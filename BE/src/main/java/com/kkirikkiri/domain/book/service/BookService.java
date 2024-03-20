198fac6bf87bf8088d1379986b9ae4d7975d227a
Juhyun Lee <jennysophia3@naver.com>
2024-03-20 14:15:04 +0900

    [Back] fix: content 생성 API 수정

--- BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:206c92b4ae8d21c13e2deb4f0c20a70796ecf181775967c866b4ea4a3b7e94b9
-size 7326
+oid sha256:ab620dcebb37cd5e91a375b0fcffef0c21889e8341acbbfa2170a39029114b23
+size 7593
