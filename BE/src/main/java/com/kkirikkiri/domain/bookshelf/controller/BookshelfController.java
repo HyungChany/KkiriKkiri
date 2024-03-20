f9e3d1111b8a6cad385d27c8424c18cd2c5cd039
Juhyun Lee <jennysophia3@naver.com>
2024-03-20 10:45:03 +0900

    [Back] feat: 전체 동화책 조회 API 개발 중
    Former-commit-id: 35451623b70f94c68352ccbab698e7ff9ddd8ac0

---
+++ BE/src/main/java/com/kkirikkiri/domain/bookshelf/controller/BookshelfController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aaa0cbeb5a16852408db4b690862f32d034a8c23186c88face71310c5c314624
+size 1054
---
+++ BE/src/main/java/com/kkirikkiri/domain/bookshelf/service/BookshelfService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3597763f899e43acde5cb7fb8f66ffac3663fbd13d2c33a9244ecda518388329
+size 1946
