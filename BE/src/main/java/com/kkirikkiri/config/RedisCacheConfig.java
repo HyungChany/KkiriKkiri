ec462376a37cdd3a6ea56fcf0b44da4450dd290a
Juhyun Lee <jennysophia3@naver.com>
2024-03-20 11:01:35 +0900

    [Back] feat: redis 설정, 적용

--- BE/src/main/java/com/kkirikkiri/config/RedisCacheConfig.java
+++ BE/src/main/java/com/kkirikkiri/config/RedisCacheConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a4b0d248d2df0e20710324559639c871c90382f114525333c29a62ac1d412dc
-size 723
+oid sha256:3fd0b31afa8ec9cb62d65e1dd453be9b42f2800044baeae0772577dc9ed55712
+size 689
