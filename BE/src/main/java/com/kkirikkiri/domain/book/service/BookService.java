82f788d647a8a73f4fc6dabfbe412eed3387b992
Juhyun Lee <jennysophia3@naver.com>
2024-03-20 16:28:57 +0900

    [Back] feat: 동화책 제목 수정 API 완성

--- BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed0442d9ee479b0f44ed4d4cc1608cf9c4cd440164f401d6a35e602ef0b57bd4
-size 7924
+oid sha256:1dd62200c14e16c3716b12244886a7a6802330e41e1c2372ee3144a89a68e366
+size 8588
