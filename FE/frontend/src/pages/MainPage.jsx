7b8a69d58fbc7f88fecf1bef872bc8713f94deb2
조형찬 <bd3945@naver.com>
2024-03-21 17:28:19 +0900

    feat: 메인페이지 수정
    Former-commit-id: 4e8e00bb4e2dea3cf8fee300207ca64fc0d187fb

--- FE/frontend/src/pages/MainPage.jsx
+++ FE/frontend/src/pages/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0631d15f95dd5ba65c93aed7a231ffa455166470228d9850038660cbe6a83fdc
-size 842
+oid sha256:ff40f4519e1936e6949f44681816d01f129b501c48fdb5ca2e51db07eb7baccf
+size 2164
