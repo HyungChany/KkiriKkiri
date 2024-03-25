52d979c467b7f38dccf202a56b840ba8aa5e7604
조형찬 <bd3945@naver.com>
2024-03-25 16:20:18 +0900

    feat: 애니메이션 조정
    Former-commit-id: f1441ced99a5433dc1340e9cac8430d892770527

--- FE/frontend/src/pages/LoginPage.jsx
+++ FE/frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7e66e5a4c55ae27006cd6937a2f41262828d89a24dfc2aa1991f7411c990aa4
-size 5315
+oid sha256:ee955c9d8c36a37fe0ad67bbb77a9987f338296c9ad191f122416db961cdb68b
+size 5177
