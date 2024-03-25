bf6dc9a7554cd23a3c27d77342f1d10e57d1d8e9
dbtywi <dbtywi@naver.com>
2024-03-25 13:48:48 +0900

    [Back] feat: 회원 request url 수정
    Former-commit-id: 934f568853ca75ef6f57b0bc79e71e947375441a

--- BE/src/main/java/com/kkirikkiri/domain/member/controller/MemberController.java
+++ BE/src/main/java/com/kkirikkiri/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93d4e2cd3a320736374e118cb60acec857901260ee19eff9feb1202f01149ce7
-size 3975
+oid sha256:e6ad78f7834bc0a7d89c7892522c008b908fe0eb9cfff05549a38cf70ecf41fd
+size 4015
--- BE/src/main/java/com/kkirikkiri/domain/member/dto/UpdateInfoRequest.java
+++ BE/src/main/java/com/kkirikkiri/domain/member/dto/UpdateInfoRequest.java
@@ -1,14 +1,3 @@
-package com.kkirikkiri.domain.member.dto;
-
-import com.kkirikkiri.domain.member.entity.enums.EnglishLevel;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UpdateInfoRequest{
-        private String password;
-        private String nickname;
-        private EnglishLevel level;
-        private String thumbnail;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:851a2f43d3a978e0c1ae6030ac5c8d3aa9eab3cd5f26854ad20d9e1a9191ba19
+size 646
