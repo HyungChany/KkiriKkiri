df9706758052abf0536da9aa50ba66e31079f5a0
Juhyun Lee <jennysophia3@naver.com>
2024-03-25 13:47:04 +0900

    [Back] style: 불필요한 패키지 삭제
    Former-commit-id: 1ccce24efd675341bc1fb666fe9c3c47c28039cc

--- BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a56df2192de548d6ffa37832c3f425c9f4d18648ee74038bd4b1ae9dbb8fd876
-size 9891
+oid sha256:742bc5f8dae3ff2a57e97bb2d9d05993ed3e4453b1705702fc46e472739fcc66
+size 9681
