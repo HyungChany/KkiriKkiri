191e39c9b684661f6453fcc2405c06386f03d7f9
류지원 <awldnjs2@naver.com>
2024-03-26 11:58:15 +0900

    [Back] fix: Story JPA 수정 & Story 상세 조회 수정 & Story 생성 수정 & Story전체조회 수정
    1. Story Table에 summery와 create_date Column을 추가하였고, create_date에는 튜플생성시 생성날짜가 자동 적용되도록 하였습니다.
    2. Story 상세조회시 summary와 create_date가 조회되도록 수정하였습니다.
    3. Story 전체조회 시 작가명이 제대로 나오지 않는 점을 수정하였고, content를 조회하여 첫번째 이미지의 URL을 반환하도록 수정하였습니다.
    
    
    Former-commit-id: 40bbb0071e265584de87804044256fa1914bf5c1

--- BE/src/main/java/com/kkirikkiri/domain/book/dto/StoryResponse.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/dto/StoryResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee3a971eb02ec51115f3801dcfd3ab7448b9a40bcf0e57b64003fbbdeac59a46
-size 712
+oid sha256:4469e852060c93bf0b82aa7debca3bdcd51d6af94b02e194a3b2a2570626da30
+size 788
