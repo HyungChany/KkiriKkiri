1ee7e5b395099ba42b0efe8887043a587fd527de
류지원 <awldnjs2@naver.com>
2024-03-27 03:25:57 +0900

    [Back] fix: 책장 전체 조회 시 summery가 반환되도록 수정
    Former-commit-id: 5f532d5dc22e91f935b679454aa6caec438cb532

--- BE/src/main/java/com/kkirikkiri/domain/bookshelf/service/BookshelfService.java
+++ BE/src/main/java/com/kkirikkiri/domain/bookshelf/service/BookshelfService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7809018aace0d6743667cf460d528ebb939d90a68d783dc987d29bee7d2a86b1
-size 2586
+oid sha256:6a87d779f30dbd4f318bbad32c0574c391db9714b757ea52993528ba43dbc549
+size 2691
