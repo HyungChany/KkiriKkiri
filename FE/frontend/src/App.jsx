11485fa221182b5fad107b64527d6e159c88441d
조형찬 <bd3945@naver.com>
2024-03-27 11:16:04 +0900

    feat: book 라이브러리
    Former-commit-id: e9f00cecb1597b4fb1313ea0b90da8f79567e1d7

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3550a3f5a9d92f85164a1f9e4a8da445c9a14d59bcd5f67e7012ff73bf8b1314
-size 1375
+oid sha256:e81b6b674e5e75477bcd3dc27d83f5693f8b94fff9b1f1ccce0eb65183d1769c
+size 1473
--- FE/frontend/src/pages/StoryBookPage.jsx
+++ FE/frontend/src/pages/StoryBookPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec76c7d2b3d1cd350ea4faf08c543e81d22b76b024b03d1ab25a05c95b2dbcf
-size 260
+oid sha256:c7ad5c2ad585d0228372eea55fd7d53843bfe4ac64f96556717532df0df172f8
+size 2450
