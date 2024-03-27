890e0b89da28268f271f687f89d6abc5f452a368
yugyeong <starinsky284@gmail.com>
2024-03-27 14:59:58 +0900

    feat: 프론트 태블릿 규격 테스트중
    Former-commit-id: 27ed2f92c041754e2cf4bbf525f5589054120bb0

--- FE/frontend/src/components/header/Header.jsx
+++ FE/frontend/src/components/header/Header.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86d49d32da6f70f51255fd3cdc10213c5c37567691bce5b81e2787c533c08cbf
-size 2803
+oid sha256:1ac0dba7770c907ae6f7590c47ab49a57a7948846d6dade40fa590812bb5ed01
+size 2858
