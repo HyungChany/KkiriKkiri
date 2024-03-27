6d6e50f916687ab41779ede3fd17e3a368c352ba
yugyeong <starinsky284@gmail.com>
2024-03-27 14:31:48 +0900

    feat: 프론트 태블릿 규격 테스트
    Former-commit-id: 56deb424574018170e7458b80873465c30f0734c

--- FE/frontend/src/pages/LibraryPage.jsx
+++ FE/frontend/src/pages/LibraryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5804c053b991a800b52695bf4235d513a1b176017265b2688bacb244ba02a72f
-size 17663
+oid sha256:508b3e33951c6d184d4c393bdc5a8902de27ccd44c5cf1ec6ebda6ed934a68a6
+size 17676
