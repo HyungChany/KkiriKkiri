5c7640cefdeeb7c2e7119d90df95283767c0b912
Juhyun Lee <jennysophia3@naver.com>
2024-03-27 10:49:49 +0900

    [Back] fix: 동화책 상세 조회 API 수정
    Former-commit-id: 24a25c7ac766f102e8f521611ea9103ca86d63e2

--- BE/src/main/java/com/kkirikkiri/domain/book/dto/StoryResponse.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/dto/StoryResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4469e852060c93bf0b82aa7debca3bdcd51d6af94b02e194a3b2a2570626da30
-size 788
+oid sha256:e4967aa61ce78e0b5c72406cd90a515f9ccdb9699694294f68c03f0c24da787a
+size 832
--- BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6d872f86e3bbe0fe047bfe7e44dc08832276260b6695158f453d28402cb959a
-size 9925
+oid sha256:ea5c9826b23f709a1ba715bf73d2604dc07d9f18cd983fb07e7d1ee67526f541
+size 9704
