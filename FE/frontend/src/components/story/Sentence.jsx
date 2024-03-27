0112044045cb1af78ba30e9098268a411453c369
주홍찬 <rokaf0743@gmail.com>
2024-03-27 17:24:36 +0900

    design: story page 크기 조정
    Former-commit-id: 1729ea9446927dd43ab977cd239565fd6a9f9ca0

--- FE/frontend/src/components/story/Sentence.jsx
+++ FE/frontend/src/components/story/Sentence.jsx
@@ -1,19 +1,3 @@
-import styled from 'styled-components'
-
-const Sentence = styled.div`
-	width: 100%;
-	min-height: 16rem;
-	height: auto;
-	border-radius: 3.125rem;
-	background-color: #FFF7E7;
-	font-size: 2.5rem;
-	font-family: "Ttangsbudaejjigae OTF";
-	font-weight: 300;
-	padding: 3rem;
-	text-align: center;
-	line-height: 4rem;
-	box-sizing: border-box;
-	margin: 1rem 0rem;
-`
-
-export default Sentence
+version https://git-lfs.github.com/spec/v1
+oid sha256:af084b711b37f235d10ab1730461a94e6477ea98f1dfb67952cd37bca8854474
+size 379
