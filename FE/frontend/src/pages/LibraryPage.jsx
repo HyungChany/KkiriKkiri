6b638b92227a24070ab55b72ed289d0a5e6b0372
yugyeong <starinsky284@gmail.com>
2024-03-28 04:43:28 +0900

    feat: 도서관페이지 태블릿 화면 규격 맞춰 길이수정
    Former-commit-id: 6a731de8ea4c04710c1798e5ec8b82d94813039f

--- FE/frontend/src/pages/LibraryPage.jsx
+++ FE/frontend/src/pages/LibraryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:508b3e33951c6d184d4c393bdc5a8902de27ccd44c5cf1ec6ebda6ed934a68a6
-size 17676
+oid sha256:e7624163465547d8944000bca439c5d5fe0f048ada93183b87c3cd92305651f6
+size 17094
