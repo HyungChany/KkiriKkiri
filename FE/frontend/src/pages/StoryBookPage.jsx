443e74f6d31e4d1ba78eeba0f57792e2858920ae
yugyeong <starinsky284@gmail.com>
2024-03-27 11:12:40 +0900

    feat: 프론트 빌드 오류수정
    Former-commit-id: 9f49d5dc4044cfa81125d2e4f2d6ea5abab0b3c1

--- FE/frontend/src/pages/StoryBookPage.jsx
+++ FE/frontend/src/pages/StoryBookPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ec76c7d2b3d1cd350ea4faf08c543e81d22b76b024b03d1ab25a05c95b2dbcf
-size 260
+oid sha256:015e2e769c9efe2595c94ad918725d3621c634799b5dfa29f61eeea1e0d983b4
+size 58
