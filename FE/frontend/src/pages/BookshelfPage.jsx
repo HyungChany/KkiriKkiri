89e4640b4fed49db2415e4c9fe0fc4e2a12180aa
yugyeong <starinsky284@gmail.com>
2024-03-28 04:44:26 +0900

    feat: 책장페이지 개발 진행 중
    Former-commit-id: c52aed7f539f067c782e33db125fec10238a8a54

--- FE/frontend/src/pages/BookshelfPage.jsx
+++ FE/frontend/src/pages/BookshelfPage.jsx
@@ -1,10 +1,3 @@
-
-const BookshelfPage = () => {
-    return (
-        <div>
-            안녕하세요
-        </div>
-    );
-};
-
-export default BookshelfPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c5d942bcd19ea7c9485aecb2672de7ac6e54ea9d2915d0fe77759b4a8dfa705
+size 2896
