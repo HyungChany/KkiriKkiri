b9fcba5042ddc71a0eb44ff07bce962ace2681c0
조형찬 <bd3945@naver.com>
2024-03-27 11:16:04 +0900

    feat: book 라이브러리
    Former-commit-id: c917980f54fe18db3aadf693bcd4ed72107c1a87

--- FE/frontend/src/pages/StoryBookPage.jsx
+++ FE/frontend/src/pages/StoryBookPage.jsx
@@ -1,15 +1,3 @@
-import Background from '../components/common/Background';
-import background from '../assets/book/backimg.png';
-
-
-const StorybookPage = () => {
-  return (
-    <Background backgroundimage={background}>
-s
-
-
-    </Background>
-  );
-};
-
-export default StorybookPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7ad5c2ad585d0228372eea55fd7d53843bfe4ac64f96556717532df0df172f8
+size 2450
---
+++ FE/frontend/src/pages/Test.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c985edd7db27f0c9d599561dbb19cfc38c35ae8cfb8eaf11c026ad67a6a040ea
+size 2612
