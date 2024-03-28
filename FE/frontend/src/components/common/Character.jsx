1cb2a5a47643819979d8004a87e7d4496d73fb16
yugyeong <starinsky284@gmail.com>
2024-03-29 07:19:50 +0900

    feat: 회원가입로직구현중
    Former-commit-id: bc0a3814aeb833f81831d3823389e0c82700ad01

--- FE/frontend/src/components/common/Character.jsx
+++ FE/frontend/src/components/common/Character.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d16721f29ff4e86d08374c31a7dc646be7d14e1569d027f8903fc11774283cea
-size 2704
+oid sha256:9fc8e4edd694b841d31177d5bb5c2aff7fea03a85e9774e51136769706fdbbd6
+size 2851
--- FE/frontend/src/pages/SignupPage1.jsx
+++ FE/frontend/src/pages/SignupPage1.jsx
@@ -1,3 +1,3 @@
-import styled from "styled-components";
-import Background from "../components/common/Background";
-import background from '../'
+version https://git-lfs.github.com/spec/v1
+oid sha256:6456981b0edd75b5e012c57e7e4af576ab21e08dc4a982017c12799e92ecb4ec
+size 9088
