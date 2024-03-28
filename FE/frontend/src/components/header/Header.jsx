470f4d20bc8273fde07ee6b829f5fbf2c7d3bf58
조형찬 <bd3945@naver.com>
2024-03-28 09:54:26 +0900

    feat: 태블릿 테스트 코드 추가
    Former-commit-id: 0effa61ec9863b1a9d8e66bbff1a31609214488d

--- FE/frontend/src/components/header/Header.jsx
+++ FE/frontend/src/components/header/Header.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ac0dba7770c907ae6f7590c47ab49a57a7948846d6dade40fa590812bb5ed01
-size 2858
+oid sha256:6ce2ff7da5939b882a438bdff697140adf7674593da46adb3f315bfec1b9c522
+size 2970
