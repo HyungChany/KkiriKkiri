3b16805e915d91b4dcd9444cd4d800a8dc44301c
주홍찬 <rokaf0743@gmail.com>
2024-03-28 18:04:39 +0900

    feat: 스토리 생성 프롬프트 수정
    Former-commit-id: 2f3e4b0b3b9b5373d2dccbde38e72c68fd292a13

--- FE/frontend/src/pages/StoryPage.jsx
+++ FE/frontend/src/pages/StoryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d659600116f98d8430e7660c016fa24505ae6543e1efa89b2454f5be2d49712d
-size 15145
+oid sha256:7c91726ce1d13efc463bee0a74eb7ae3bf0b8480fc3dada1180a84259f5778ca
+size 14448
