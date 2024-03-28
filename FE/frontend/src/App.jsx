eadb24f69bf25404f585f638109a06a1745ad7c9
yugyeong <starinsky284@gmail.com>
2024-03-28 15:03:27 +0900

    책장페이지 구현중
    Former-commit-id: aa95759bb8f3423e474e6c4d117cf9642b49429c

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3156c98aa9377b620e7d7397221b459a129c6787530ef0dad2d1b1eb583df4c4
-size 1700
+oid sha256:adcaf7a5da99c041a7fc6e72d08c9dc9ab0f2fcca6e76cce3d0bf62555809877
+size 1803
