b049a9f0f98e0218ac6ae03c3e53a84194fef251
dbtywi <dbtywi@naver.com>
2024-03-28 15:25:55 +0900

    [Back] fix: 동화책 전체 조회 API에 학습여부 확인 데이터 추가
    Former-commit-id: b3f6965fec46215f8f9ae36ea44ab63edbee49d4

--- BE/src/main/java/com/kkirikkiri/domain/learning/repository/LearningRepository.java
+++ BE/src/main/java/com/kkirikkiri/domain/learning/repository/LearningRepository.java
@@ -1,13 +1,3 @@
-package com.kkirikkiri.domain.learning.repository;
-
-import com.kkirikkiri.domain.learning.entity.Learning;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface LearningRepository extends JpaRepository <Learning, Long> {
-    List<Learning> findAllByStoryId(Long storyId);
-
-    Optional<Learning> findById(Long learningId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:242b11f56be9c1672924972c233898a327d74caac63e3c6495a78319ef183e42
+size 481
