746544f3d556d274b10bff833aa79a7cfe3cd140
주홍찬 <rokaf0743@gmail.com>
2024-03-28 15:02:47 +0900

    fix: 이야기 페이지 UI 수정
    Former-commit-id: e926b0ec7cd45876944d6845705dac275aba1004

--- FE/frontend/src/components/story/Sentence.jsx
+++ FE/frontend/src/components/story/Sentence.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af084b711b37f235d10ab1730461a94e6477ea98f1dfb67952cd37bca8854474
-size 379
+oid sha256:c7455e4b185c1fb1efb7554f59c4c7b343bc478ef08728ef08eb08c7485aa282
+size 423
