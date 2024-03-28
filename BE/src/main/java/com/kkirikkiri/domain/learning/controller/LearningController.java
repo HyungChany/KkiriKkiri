9480c09f92eb382dc813e9fc5c8a9f3deae81cf2
dbtywi <dbtywi@naver.com>
2024-03-28 10:44:32 +0900

    [Back] fix: 공부 초기 데이터 생성 API 수정
    Former-commit-id: 2e59355196e7bedf59954a1bc949fb359c1dbe6d

--- BE/src/main/java/com/kkirikkiri/domain/learning/controller/LearningController.java
+++ BE/src/main/java/com/kkirikkiri/domain/learning/controller/LearningController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38a9bcf7669c9ec921dde06b66f4a3e4a4ed9305f15f456790c6702d1bdc2bf6
-size 1819
+oid sha256:223acac6ec4e5a5cc6e84cf6a246d20ab51c0b2ee6bb1d1daa5d821fcf878f71
+size 1941
