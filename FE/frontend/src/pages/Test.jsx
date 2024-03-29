7e50d17d9996ec21cb38206300e9871c9f258dc4
조형찬 <bd3945@naver.com>
2024-03-29 12:07:59 +0900

    fix: 동화책 임시 테스트
    Former-commit-id: a84c1556bfa6edaa8fc02d27e34bf7f1b1f5ec45

--- FE/frontend/src/pages/Test.jsx
+++ FE/frontend/src/pages/Test.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:190894ac61b4f3df6f0653f31a70d6b342f0e2365e113f060dbd388a6bad9a65
-size 3372
+oid sha256:b448e2d6fbfa7cccd58644b32327089b9f9028440b3ecf9e0865969799371e23
+size 5555
