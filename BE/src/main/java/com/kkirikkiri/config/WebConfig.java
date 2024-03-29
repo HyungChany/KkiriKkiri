67ba7fcc9b0630cd988ff608649bab6a436b894d
dbtywi <dbtywi@naver.com>
2024-03-29 10:34:56 +0900

    [Back] fix: CORS 권한 설정 수정
    Former-commit-id: 326d7c9a985af1d265f4f9d0dc4b7d871ed2eaf3

--- BE/src/main/java/com/kkirikkiri/config/WebConfig.java
+++ BE/src/main/java/com/kkirikkiri/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:103963457fcc29f5330049613ab6c7da8f33f0a8f45c400d129cff80622c82fb
-size 748
+oid sha256:1dfb426c1f12ad521ac406f4c701851b63781c59c0e080851487b743b505d7fc
+size 1463
