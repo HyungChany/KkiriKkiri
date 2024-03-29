ce76d73a8750291d5e15339c6c9375dfb4cae283
dbtywi <dbtywi@naver.com>
2024-03-29 19:52:06 +0900

    [Front] feat: 책장 페이지 데이터 연결 및 모달 구현
    Former-commit-id: d26001a0018481d00f4f570c4759ea515efdb84b

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adcaf7a5da99c041a7fc6e72d08c9dc9ab0f2fcca6e76cce3d0bf62555809877
-size 1803
+oid sha256:cacc16a0839b8c7b6f145ee6693735628509c797e8e7eab2e499ff17dd5f4b2e
+size 1812
