8ae4d036f2907d610183cd48fc1188a5346fc20e
dbtywi <dbtywi@naver.com>
2024-03-29 20:46:34 +0900

    [Back] fix: 도서관 전체 조회 API output 데이터 추가
    Former-commit-id: 3a8f04bc5e5cccd62133143c7739a42df7e37994

--- BE/src/main/java/com/kkirikkiri/domain/book/dto/LibraryResponse.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/dto/LibraryResponse.java
@@ -1,20 +1,3 @@
-package com.kkirikkiri.domain.book.dto;
-
-import lombok.Builder;
-import lombok.Data;
-
-import java.time.LocalDateTime;
-
-@Data
-@Builder
-public class LibraryResponse {
-
-    private String title;
-    private String author;
-    private String summary;
-    private String imageURL;
-    private Integer download;
-    private LocalDateTime createdAt;
-    private boolean isMine;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7ebbd4cb7d2bf11e526cd4c59be926d8b4547bbc5efc7c9d5128e83abcd3718
+size 399
