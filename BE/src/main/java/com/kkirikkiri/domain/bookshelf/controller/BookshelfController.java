7dc0331ff48009ae994044d7fde00bcfee6c38ec
류지원 <awldnjs2@naver.com>
2024-03-29 09:49:34 +0900

    [Back] fix: 모든 Controller RequestMapping에 Nginxv프록시용 "/api" 추가
    Former-commit-id: d3101dd7d46f674a4c7586087efd6c2cfe82c142

--- BE/src/main/java/com/kkirikkiri/domain/bookshelf/controller/BookshelfController.java
+++ BE/src/main/java/com/kkirikkiri/domain/bookshelf/controller/BookshelfController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbf0bb9482d972f395c2ba2deefd1fbdb8b2043dc1e945253956a5a944869e20
-size 1441
+oid sha256:b72108a97bd39d81465a4b698903357c278bdcfbbaf01198bc1497d4dee3b19d
+size 1445
--- BE/src/main/java/com/kkirikkiri/domain/learning/controller/LearningController.java
+++ BE/src/main/java/com/kkirikkiri/domain/learning/controller/LearningController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e442f3d28f474fb078124e3da86c2d5e8027bf233c5b49f5f84ca5f358f85724
-size 2029
+oid sha256:58af7235c68a3f7140216eb178f17b53d9204d8ed305ca89f9eac6d330804e0d
+size 2033
