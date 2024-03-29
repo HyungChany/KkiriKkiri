41640101bc4b2fcc7d2fb5acde249e1bab4e82de
류지원 <awldnjs2@naver.com>
2024-03-29 09:20:26 +0900

    [Back] fix: Nginx 테스트 코드
    Former-commit-id: 88f6be9bc37cf0e7ed4aeb2171978e16043f995f

--- BE/src/main/java/com/kkirikkiri/domain/book/controller/TestController.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/controller/TestController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc6a12f74e77ccf38d49c5908e2f20602fbf08f5821e06d7a956fca97a6e371f
-size 1100
+oid sha256:591e81b3716c5613cb1e9217de0145f408df5b6d43e710edf22be8b3b098ffb5
+size 1108
