bd5824efbf6d90fad33a18da7d0163a183ac5380
류지원 <awldnjs2@naver.com>
2024-03-31 15:07:34 +0900

    [Back] feat: 이미지 삭제 코드추가 및 gitignore 추가
    Former-commit-id: c940d435410b28e01ee0dfd635561f9642708a11

--- AI/app.py
+++ AI/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1c18772301c6adb240ce65f75e17d9d82cf1c7a16c6549dbef784cc20715059
-size 2136
+oid sha256:c823eddcc1916223ca3e2a7bb704c85e48707a6f58407ea3ee0351d7df7e970f
+size 2825
