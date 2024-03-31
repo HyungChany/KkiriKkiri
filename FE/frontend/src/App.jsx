2e6789560a966c15e5f802d29faa611149c1b6d9
조형찬 <bd3945@naver.com>
2024-03-31 23:18:52 +0900

    feat: CSS 수정
    Former-commit-id: f0359e1275c424357aaf9004e2e6e4f8e066be42

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0487fcf771b6af126bd856081dd356c89f4a16241663f9dc6c18a689109511b0
-size 1829
+oid sha256:eb615443509de09b34e6e2530eaa2c1b99063d10b204a4d709dbf188eb2cc56f
+size 1828
