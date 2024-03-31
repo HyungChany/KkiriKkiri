b88bd3a85cac7773a0210c7ec489510610833cf9
조형찬 <bd3945@naver.com>
2024-04-01 00:33:46 +0900

    fix: 로그인 회원가입 CSS 수정
    Former-commit-id: 4b449f054c49ec25ac5afa93915890b22e21a5d5

--- FE/frontend/src/pages/LoginPage.jsx
+++ FE/frontend/src/pages/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1f3f8f903013c7d36d5bbebad0d233faf1d1d8e25488d1f752079d8a210c55f
-size 7051
+oid sha256:5abd84fccb7277277a947b02137293581b813c3a471478d38204bb6edce8ea94
+size 7106
