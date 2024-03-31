81ba178e79f34afdec01e554d36d01f0bf1fb744
류지원 <awldnjs2@naver.com>
2024-03-31 17:31:26 +0900

    [Back] fix: GPU사용코드 주석처리
    Former-commit-id: 6cad874edb9e61ab94ed36f823b9d0040aaed381

--- AI/app.py
+++ AI/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c823eddcc1916223ca3e2a7bb704c85e48707a6f58407ea3ee0351d7df7e970f
+oid sha256:161b6fedc7733271e2bfd446bef81875ae64ff0edbc59ac594804f695addca28
 size 2825
