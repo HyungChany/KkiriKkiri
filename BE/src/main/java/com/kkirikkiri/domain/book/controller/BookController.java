a5eeb4900cecad829d1a9dc81d1ce7abd74acbe6
dbtywi <dbtywi@naver.com>
2024-03-31 15:16:58 +0900

    [Back] feat: 동화책 공개로 상태 변경 API 완료
    Former-commit-id: f817a53a3134c8d7cbcafbbdbc7110aae3eb811f

--- BE/src/main/java/com/kkirikkiri/domain/book/controller/BookController.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c34b7a01dda2f4459e64e66f8775d0e024d460f600203cde2670411dcf965857
-size 2755
+oid sha256:3f797a49d555b3c56afc0e50b72833de8b5e2eb123637b9d37755a3e48c8ac09
+size 2973
