c1affe7aee46210ad44c9a7193c9c98fd12c5d21
조형찬 <bd3945@naver.com>
2024-03-31 19:22:14 +0900

    fix: 헤더 수정
    Former-commit-id: b697bc1b0b77049069c0d8ca0f84ddb5d990f012

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:843cb73b836856f67a4c3c5a96d6d92857b45e79903aacb14af2131bc3d66f69
-size 1813
+oid sha256:0487fcf771b6af126bd856081dd356c89f4a16241663f9dc6c18a689109511b0
+size 1829
