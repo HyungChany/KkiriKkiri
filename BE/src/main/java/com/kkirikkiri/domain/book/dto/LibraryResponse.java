99a590152d9981f091218e79352bbdfca58cc498
Juhyun Lee <jennysophia3@naver.com>
2024-04-01 09:52:33 +0900

    [Back] fix: 도서관 조회 API 수정
    Former-commit-id: 32e49e9bc9ca0672fa9e68e05bda4962c797f5bd

--- BE/src/main/java/com/kkirikkiri/domain/book/dto/LibraryResponse.java
+++ BE/src/main/java/com/kkirikkiri/domain/book/dto/LibraryResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7ebbd4cb7d2bf11e526cd4c59be926d8b4547bbc5efc7c9d5128e83abcd3718
-size 399
+oid sha256:a308f0d2f1c551fb1aff66bef53d9e9fe4de7414eef9de2429b0ad3ca717245d
+size 433
