b07f1362ae8dd627bc3a1d72bc49d3354639fa44
dbtywi <dbtywi@naver.com>
2024-04-01 17:22:19 +0900

    [Front] fix: 프로필 사진 s3 연동
    Former-commit-id: 2298c1ce337fc84bc7cf6cb723152fad6b9360bb

--- FE/frontend/src/components/header/Header.jsx
+++ FE/frontend/src/components/header/Header.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec15d51ccdf288b7392ac8ff78693ebeb126aff2713cdfafb4ab666102c2b94a
-size 4929
+oid sha256:455503eb3280494f805955da03ce4d6b4980c94ad89300028c3f1ae407b26477
+size 5039
--- FE/frontend/src/pages/SignupPage.jsx
+++ FE/frontend/src/pages/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35f9e837ee270d79e2beb5f239b4b8e44c00a4ddcfc52f606d21cc4966543757
-size 14526
+oid sha256:668edb3a2ba27fc0ef3ce8b4e281d1565c745e8632f3f97cc2e1d6b3e0273dc2
+size 14579
