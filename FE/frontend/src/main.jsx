9ef4b409ed8c5a8960bf799bc8a887dc5f21355a
류지원 <awldnjs2@naver.com>
2024-04-01 17:13:30 +0900

    fix: 회원가입 유효성검사 및 CSS수정
    Former-commit-id: 665bfd2a36785a914a01a5b8fe36241a931dc693

--- FE/frontend/src/main.jsx
+++ FE/frontend/src/main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b97d40d9c3905e6ecdc05255928a1e519c95ed796f4d1b0c7c0e29c91aeb45da
-size 792
+oid sha256:ee8634c9e51c0a14f4a3448ee08eb277c30dabfa7c410e2e622caafcfa08ca91
+size 759
--- FE/frontend/src/pages/SignupPage.jsx
+++ FE/frontend/src/pages/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c78a87c6f2eccfaa72296714ee7fa2f788c576abf8e2304a3be5371f44b7ac8
-size 14074
+oid sha256:24caf07c5530bb9872c6b616b8e8ff8bc4dddd1aa886a3271421b2163ffd069b
+size 17248
