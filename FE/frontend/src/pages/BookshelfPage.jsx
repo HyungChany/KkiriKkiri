d74cde17e5a44f630b8a254e39a9cc1910b3adb1
조형찬 <bd3945@naver.com>
2024-04-01 14:30:51 +0900

    fix: 백그라운드 배경 확장자 변경
    Former-commit-id: c36ef98bce4823e8ba4aa995694e86d7393cd90f

--- FE/frontend/src/pages/BookshelfPage.jsx
+++ FE/frontend/src/pages/BookshelfPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44e308ecba7c77fecb3104ee4fdba09baf70a6ed9ef41540baa612dfe2036928
+oid sha256:659cc0df4e3613e31a8c06d68edeeba9ed5f3994374f52fc0e2a7f8b89a320ec
 size 11669
