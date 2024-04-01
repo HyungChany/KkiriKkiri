ea8c9a9524dee740e607f10d4aaadafd01c15412
주홍찬 <rokaf0743@gmail.com>
2024-04-01 16:52:03 +0900

    feat: 이야기 페이지 api연결, 책장 페이지에 자기작성 동화책 추가
    Former-commit-id: 8a5e1ca1ac03d9c32a37ff4ba016887ce1df77d5

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09da14a442ddcea2f083399a93b3aeab8b352f98de2f25853bf9f0fea52e162e
-size 1863
+oid sha256:dac77f24501f1ddea08dd0726fd36148997e9fbd59f862bd3f500cbd5778cc29
+size 1864
