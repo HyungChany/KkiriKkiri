31a268c33c79889a983d976eb2a7f01545622b19
yugyeong <starinsky284@gmail.com>
2024-04-01 11:57:27 +0900

    feat: 닉네임글자수제한
    Former-commit-id: 7a2e8d141b3ac407e0f14a315da0007ac8769716

--- FE/frontend/src/pages/SignupPage.jsx
+++ FE/frontend/src/pages/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c78a87c6f2eccfaa72296714ee7fa2f788c576abf8e2304a3be5371f44b7ac8
-size 14074
+oid sha256:95585006a929256becef88a4d67054dc8eecb1bea29e0add13cdd2c1ad3e4bc6
+size 14526
