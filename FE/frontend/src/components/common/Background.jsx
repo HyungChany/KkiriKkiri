0e256990cc1e9529ee9780fe8fc47c03729922dc
조형찬 <bd3945@naver.com>
2024-04-01 16:29:52 +0900

    fix: 백그라운드 배경화면 오류 수정
    Former-commit-id: 41031ea51732767c3856007a6bba434407b8922f

--- FE/frontend/src/components/common/Background.jsx
+++ FE/frontend/src/components/common/Background.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12d47248ec1119cef8a354d20059d3fd0cb57b76fca92c7352c8905b0c138c1c
+oid sha256:58aa2898a0bb0e22b1014c925933817832387216d70228b1b148df53e0ccb30d
 size 374
