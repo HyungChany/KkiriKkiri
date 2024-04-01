6340a6fd4bb207bca36a957dca4cf6a12b6e1788
yugyeong <starinsky284@gmail.com>
2024-04-01 11:12:48 +0900

    feat: 경로변경
    Former-commit-id: 62a5da1b9ce133523ea902e487b5dab0ddb1306a

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ac284787fa1a179c29e10f613570d0e0f8d61d3f619a8338cf393a60c258bd8
-size 1866
+oid sha256:09da14a442ddcea2f083399a93b3aeab8b352f98de2f25853bf9f0fea52e162e
+size 1863
