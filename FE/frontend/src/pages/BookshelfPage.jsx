6ecbb26b9212ef67737eac14824e3e65e51b49da
조형찬 <bd3945@naver.com>
2024-04-01 12:20:10 +0900

    fix: 책장 CSS 수정
    Former-commit-id: 382c5a8b2758c70c031c2228ff509448945e10a5

--- FE/frontend/src/pages/BookshelfPage.jsx
+++ FE/frontend/src/pages/BookshelfPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c74d39b38f003b6c7063c8e5ad5117b9041173a7b86fbde5beecd3fd32f1111
-size 11791
+oid sha256:44e308ecba7c77fecb3104ee4fdba09baf70a6ed9ef41540baa612dfe2036928
+size 11669
