1e88770f8ede266e920fbce786743b6dd5523ead
RupertJoo <rokaf0743@gmail.com>
2024-04-01 09:57:45 +0900

    이야기페이지 api연결 진행중1
    Former-commit-id: 8a378edb2aaad0ebecaf730f938abf1ad0c70c7f

--- FE/frontend/src/pages/StoryPage.jsx
+++ FE/frontend/src/pages/StoryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0afa80835d415fa76b0d85a44f1fcafaaaefde3ce2e54ab5ae5156a2ef8c4f8
-size 16986
+oid sha256:b81bd4fd3cb5c36f4bf1ca8b43463d680fd8ffffdd2aeff97fc87f0a758cf873
+size 21112
