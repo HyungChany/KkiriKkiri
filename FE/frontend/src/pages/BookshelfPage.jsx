d1ad7f74638c6b208b3fe618e4abe89acc677888
류지원 <awldnjs2@naver.com>
2024-04-03 01:08:19 +0900

    fix: : 모달페이지 버튼 CSS 수정
    Former-commit-id: 81919372986a0802acbbdb0834ab88338e2119f3

--- FE/frontend/src/pages/BookshelfPage.jsx
+++ FE/frontend/src/pages/BookshelfPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a285cddfe681c68cd792c7c3da21e2c6553e56f86cf2db0a19d5475771082541
-size 12956
+oid sha256:7e4ed260bcf5f2e35909e22c8db1edab6a0bddbc52b21c4cd4c9bbaf0684c3bd
+size 12950
