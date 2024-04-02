11338d8452678969431599912753fa0822428c30
류지원 <awldnjs2@naver.com>
2024-04-03 03:36:19 +0900

    fix: 회원정보 수정버튼 위치 수정
    Former-commit-id: 576a30780c208d5dd1e32e36743969514909a2b9

--- FE/frontend/src/pages/MypagePage.jsx
+++ FE/frontend/src/pages/MypagePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:263c6798dac013e405befae4c3d09e7b4d3fa544baabf50f679a46ae0c255d1c
-size 16499
+oid sha256:53bc1846f3df96e334f08d2ddb6c808de5adca52ccfad5a1ad9ce744d9b2ec92
+size 16496
