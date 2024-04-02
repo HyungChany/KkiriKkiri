5a5abd24257e262fcaa5a66cc34f12a0bb3fdc33
류지원 <awldnjs2@naver.com>
2024-04-02 12:07:58 +0900

    fix: 마이페이지 데이터 유효성검사, 회원수정API추가, 회원 탈퇴 코드 추가
    Former-commit-id: 1c0d1e554c2bdd7527dbc1ca535e9807f2d373c8

--- FE/frontend/src/pages/MypagePage.jsx
+++ FE/frontend/src/pages/MypagePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05fa5d205891e8bf26d12a9bb5292ef0114981c9d1639e5134bf6b1365209e39
-size 8604
+oid sha256:9897de9d727fff258fc76ba3f4318f09438bbb20f3045b19fbd787f4a38c0967
+size 14376
