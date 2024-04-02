44b13b1ab59e8aefb037a2efef2c6147fd7506cd
조형찬 <bd3945@naver.com>
2024-04-03 03:39:57 +0900

    fix: 스토리 수정
    Former-commit-id: 6cf7fc77bcf905051c93ba32c574b586e9c07f1a

--- FE/frontend/src/components/Modal/StoryNameModal.jsx
+++ FE/frontend/src/components/Modal/StoryNameModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5630088c6b9e35ba712a093bc1e9d7f06ed08e9e0b77f0b58b804a449aee9429
-size 601
+oid sha256:102e2932a0ee362cca545ac3c5bedbf735be41e97758aaef5fbd3da44522e813
+size 587
