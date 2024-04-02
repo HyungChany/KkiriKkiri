c68fd50f3dab2d1555355ac46baf2bcd49689d78
류지원 <awldnjs2@naver.com>
2024-04-03 00:50:59 +0900

    feat: 그림책보기버튼 라우터적용, 스토리북 삭제 기능 추가, 랜더링 속도 개선
    Former-commit-id: 7441b781cc4f67254f26fd6c2142077713516c97

--- FE/frontend/src/pages/BookshelfPage.jsx
+++ FE/frontend/src/pages/BookshelfPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e578f92d371f038b2c1ad537dc0a4c78a8e44b8a8619d71fefddc88935fff3f
-size 10606
+oid sha256:4a54802eba057c7e4be4d92fe6330ed3540993a856f9472e5ba3077913e69181
+size 13577
