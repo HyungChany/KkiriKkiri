5b82926257b6dc84d53a5ba3bf9bf4cb0b692d5b
Juhyun Lee <jennysophia3@naver.com>
2024-04-02 22:55:48 +0900

    [Back] fix: 쓰기 학습 API 수정 완료
    Former-commit-id: 8d68eaa7c7e890da57cfcb4b1dca99730e7a443e

--- BE/src/main/java/com/kkirikkiri/domain/learning/service/LearningService.java
+++ BE/src/main/java/com/kkirikkiri/domain/learning/service/LearningService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:174ac9121986ddecc628bea59feadf6501943f30cdfc57ba4deaefbbaffa845d
-size 5012
+oid sha256:bc0a150d16161a303b44148e899d2d8e28a9a18ed8dedfa9b66ffef5d1475d60
+size 5055
