0503ed1565339e556bcc3140d9c451bb1dfa1f25
dbtywi <dbtywi@naver.com>
2024-04-03 05:16:29 +0900

    [Front] fix: 스토리 공개 여부 모달 추가 & 제목 수정 api 수정
    Former-commit-id: 15a45ef707e40b44fe904d65e03e9ab1bd400b5e

--- FE/frontend/src/components/Modal/StoryNameModal.jsx
+++ FE/frontend/src/components/Modal/StoryNameModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5630088c6b9e35ba712a093bc1e9d7f06ed08e9e0b77f0b58b804a449aee9429
+oid sha256:f96dfa37635918f934d36c061dd6f4a0116d1fc30943ca4309a85bf2c3d021f5
 size 601
