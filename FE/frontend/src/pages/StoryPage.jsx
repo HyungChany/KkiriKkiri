f4e88df80640ca43f03999b06a76b1896e49d881
dbtywi <dbtywi@naver.com>
2024-04-03 05:16:29 +0900

    [Front] fix: 스토리 공개 여부 모달 추가 & 제목 수정 api 수정
    Former-commit-id: 8162521fe05acacdae456df31e174625754e0433

--- FE/frontend/src/pages/StoryPage.jsx
+++ FE/frontend/src/pages/StoryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e4c5284376e761aec0f53da91291b2e51d41a92e16a4222df344312de856ac6
-size 27491
+oid sha256:e0a7c93d5077ae8b9f8cb1e0eda20648374e1416e4e101b55277d0be0a457f2f
+size 28450
