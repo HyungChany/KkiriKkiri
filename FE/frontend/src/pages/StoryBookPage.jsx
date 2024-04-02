1bc332c7d9bfe6de155bc58831e14fca89cba5a3
yugyeong <starinsky284@gmail.com>
2024-04-03 00:43:14 +0900

    feat: 동화책 페이지(음성, 이미지, 스크립트, 한/영 토글 기능 구현 , CSS 일부 수정)
    Former-commit-id: d6572e984bb3ea6296ac98429591218ca94f520f

--- FE/frontend/src/pages/StoryBookPage.jsx
+++ FE/frontend/src/pages/StoryBookPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c06ddc0e243ac491342843d1086f4180eada3be43f8f87efc86a526d8790b06d
+oid sha256:e81703d1bb47b61dd7326a18f191030c5ef5c4cc6ca44f69444344e5f460f134
 size 7002
