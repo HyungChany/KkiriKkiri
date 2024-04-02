da267400abeaf16c70363208f97a7b39a913aaba
류지원 <awldnjs2@naver.com>
2024-04-03 03:15:50 +0900

    fix: Header bottom padding 제거, 카드 div 아래로 내림
    Former-commit-id: 2942ee1ad6ad5c64f0c027595696481f399e39a0

--- FE/frontend/src/pages/BookshelfPage.jsx
+++ FE/frontend/src/pages/BookshelfPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e4ed260bcf5f2e35909e22c8db1edab6a0bddbc52b21c4cd4c9bbaf0684c3bd
-size 12950
+oid sha256:bd79be1ac4f14f3d1c7ed3873e86322db29a16b9177fd99faac502bfde22bbe8
+size 12807
