0d274c477dc00a7508b207d7bd6ff3cb55d18d0f
류지원 <awldnjs2@naver.com>
2024-04-03 01:46:23 +0900

    fix: header수정, 불필요한 네비 제거
    Former-commit-id: 1ddb3119adb2d603ba3b8492117b3fac20bdad7d

--- FE/frontend/src/components/header/Header.jsx
+++ FE/frontend/src/components/header/Header.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:891bd3738d98a12a2c640e4dccdfcc9c6b44c4bf644fad0b46bc3b15346623f0
-size 5176
+oid sha256:b51a6cd60f57a0344805074b84d9bdc602c1d20540e9a3ef7629a871243d6fb1
+size 4775
