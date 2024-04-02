fcb5cc14ab2a4cd86058c76e245e777b0c026cac
Juhyun Lee <jennysophia3@naver.com>
2024-04-02 23:12:10 +0900

    [Back] fix: 말하기 학습 API 수정 중
    Former-commit-id: 6c8010405fe7cd6e449957dd6803aa7a1b40166c

--- BE/src/main/java/com/kkirikkiri/domain/learning/controller/LearningController.java
+++ BE/src/main/java/com/kkirikkiri/domain/learning/controller/LearningController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3db8f3382078513d16bba1abf5743caac825244959532515cba7e6a6ba44906c
-size 2009
+oid sha256:ef0b1ae36afee7535b8811274d863b5b8d15e7a15e22d4edc5d7b8911786cad1
+size 2012
