37559f4e1df83d63aa9f177c8c78ed42aaa20989
yugyeong <starinsky284@gmail.com>
2024-04-02 15:28:36 +0900

    feat: 도서관페이지 수정, 소장하기 라우터 동적경로 추가, 동화책 페이지 구현
    Former-commit-id: 345e73ee200cd8e8fb2e14683bef7ff150ecf223

--- FE/frontend/src/App.jsx
+++ FE/frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09da14a442ddcea2f083399a93b3aeab8b352f98de2f25853bf9f0fea52e162e
-size 1863
+oid sha256:a83a952943d781a4c96cbb73293c183352f3e06e4cf8735d8f4ac01dd0d1350d
+size 1858
