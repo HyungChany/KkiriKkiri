956a38b72778c29353ac26c3cd1c4bd2c0ead660
류지원 <awldnjs2@naver.com>
2024-04-02 09:15:33 +0900

    fix: 마이페이지 초기 유저데이터 삽입
    Former-commit-id: fe4084507f2185e60b6770f6f5279870a735fae3

--- FE/frontend/src/pages/MypagePage.jsx
+++ FE/frontend/src/pages/MypagePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e031ce0a976209b4bc83ed860bd9faeb56f93809ea6045baca5a7fe372c56335
-size 6462
+oid sha256:05fa5d205891e8bf26d12a9bb5292ef0114981c9d1639e5134bf6b1365209e39
+size 8604
