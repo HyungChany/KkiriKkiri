b36eec18725125ca5e1cbb26f20e7f8e03ffba14
류지원 <awldnjs2@naver.com>
2024-04-03 21:59:25 +0900

    fix: 특수문자의 유무 상관없이 인식 기능 추가, 띄어쓰기가 인식되지 않도록 추가.
    Former-commit-id: b35e3e45f98b1e22402dfe34704bb0e83a5f87b5

--- FE/frontend/src/pages/WritingStudyPage.jsx
+++ FE/frontend/src/pages/WritingStudyPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d0ec63d4b3c25b3a8d1af663d1d4711b15e69c741161af239febfbd745f048b
-size 17813
+oid sha256:f6b26ca66d940ba0a5c79090195f62c0c09b8b1e7be8b5d0bf1a75ce1bd77f23
+size 18264
