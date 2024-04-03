93f5c94e6bc8de17ff7097c74bca3b4bb6ee8e70
박예분 <dbtywi@naver.com>
2024-04-03 11:48:33 +0900

    [Back] fix: 책장 조회 isLearned값 수정
    Former-commit-id: b0d28919ca6901978779528584758b65255b8d5d

--- BE/src/main/java/com/kkirikkiri/domain/bookshelf/service/BookshelfService.java
+++ BE/src/main/java/com/kkirikkiri/domain/bookshelf/service/BookshelfService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14b27a55756d39355a469c095e27a4d7f9dd4d6ae9bdd7f2a797c863d94a63c6
-size 5165
+oid sha256:f48a4c034cb0ad9bed959e647d1f9494e8427320b2178e7f8b2c28ea166e3eec
+size 4993
