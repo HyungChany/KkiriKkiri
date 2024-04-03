aadc1ebef9740b4998b1e6102ec0ec61bcd6e5be
류지원 <awldnjs2@naver.com>
2024-04-03 21:05:54 +0900

    fix: write 시 대소문자 상관없이 인식하도록 적용
    Former-commit-id: ec2038ff3ee101b9d1d38fbe56797eae535bbe9e

--- FE/frontend/src/pages/WritingStudyPage.jsx
+++ FE/frontend/src/pages/WritingStudyPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dacfabd86db7a1bbff7d5be138b9855761258feccdb7bdfc7b90c95f62da0bf
-size 18158
+oid sha256:8d0ec63d4b3c25b3a8d1af663d1d4711b15e69c741161af239febfbd745f048b
+size 17813
