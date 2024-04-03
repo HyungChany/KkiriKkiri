ffd8a2b5f66d56cb65b435a829c02d089ff1007e
yugyeong <starinsky284@gmail.com>
2024-04-03 21:22:15 +0900

    feat: 이야기 글자수 제한, TTS하울링 오류 수정
    Former-commit-id: d356c0c9c91ba53048ec76e86600018b87280251

--- FE/frontend/src/pages/StoryPage.jsx
+++ FE/frontend/src/pages/StoryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd313be6141a2c1cefec253408b4f4032c5ea6e4f7733d94291eeba1480ce34d
-size 32306
+oid sha256:a4c2b5daba70911d3396919fbe999e88552d21157b08d70d880580735491e8df
+size 32308
